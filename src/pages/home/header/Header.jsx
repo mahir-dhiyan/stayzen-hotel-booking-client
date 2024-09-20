076e0cc0318c82673ee858854f55a4c01931e1ed
mahir-dhiyan <mahird.chowdhury@gmail.com>
2024-09-20 13:19:02 +0600

    banner sectino photo added

--- src/pages/home/header/Header.jsx
+++ src/pages/home/header/Header.jsx
@@ -4,9 +4,10 @@ import { Typewriter } from 'react-simple-typewriter';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
-import slider1 from '../../../assets/juteProd1.jpeg';
-import slider2 from '../../../assets/juteProd2.jpeg';
-import slider3 from '../../../assets/woodenshelf.jpeg';
+import slider1 from '../../../assets/h1.jpeg';
+import slider2 from '../../../assets/h2.jpeg';
+import slider3 from '../../../assets/h3.jpeg';
+import slider4 from '../../../assets/h4.jpeg';
 // import './styles.css';
 
 // import required modules
@@ -54,6 +55,23 @@ const Header = () => {
                     <div
                         className="relative flex  justify-center items-center h-full min-h-80 bg-center bg-cover "
                         style={{ backgroundImage: `url(${slider3})` }}
+                    >
+                        <div className="absolute  inset-0 bg-gradient-to-t from-black to-black opacity-30"></div>
+                        <h1 className='md:text-7xl text-4xl z-10 font-semibold text-center text-[#ECF2EF]'>
+                            <Typewriter
+                                words={['Experience the Jute and Wood Magic']}
+                                loop={0 | false}
+                                cursor
+                                cursorStyle="_"
+                                typeSpeed={70}
+                                deleteSpeed={10}
+                                delaySpeed={1000}
+                            />
+                        </h1></div></SwiperSlide>
+                <SwiperSlide  >
+                    <div
+                        className="relative flex  justify-center items-center h-full min-h-80 bg-center bg-cover "
+                        style={{ backgroundImage: `url(${slider4})` }}
                     >
                         <div className="absolute  inset-0 bg-gradient-to-t from-black to-black opacity-30"></div>
                         <h1 className='md:text-7xl text-4xl z-10 font-semibold text-center text-[#ECF2EF]'>
