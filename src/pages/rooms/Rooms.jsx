0244a8f5cea71b0b1a5872b6a3d8bb9bf1ca7b9f
mahir-dhiyan <mahird.chowdhury@gmail.com>
2024-10-12 17:34:22 +0600

    rooms section done without reviews

--- src/pages/rooms/Rooms.jsx
+++ src/pages/rooms/Rooms.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import RoomsCard from './RoomsCard';
 
 const Rooms = () => {
+    const [rooms,setRooms]=useState([]);
+    useEffect(()=>{
+            fetch("http://localhost:5000/rooms")
+            .then(res=>res.json())
+            .then(data=>{
+                
+                setRooms(data);
+            })
+    },[])
     return (
         <div>
             <div className="bg-[#ECECF2] text-center mt-4 dark:bg-gray-300 p-4 rounded-3xl border-dashed border-2 border-[#C56652] ">
                 <h2 className="text-5xl font-semibold">Available<span className="text-[#C56652]">Rooms</span> </h2>
             </div>
+            {
+                rooms.map(room=>(
+                    <RoomsCard key={room._id} room={room}></RoomsCard>
+                ))
+            }
             
         </div>
     );
---
+++ src/pages/rooms/RoomsCard.jsx
@@ -0,0 +1,26 @@
+
+const RoomsCard = ({ room }) => {
+    const { room_name, description, price_per_night, room_size, availability, image, special_offers } = room;
+    return (
+        <div className="card bg-[#ECECF2] flex md:flex-row m-4 dark:bg-gray-500 shadow-xl">
+            <div className="  md:w-64 md:min-w-64   h-60">
+                <img className=" w-full  h-full object-cover md:rounded-l-xl rounded-xl md:rounded-none "
+                    src={image}
+                    alt="products" />
+            </div>
+            <div className="card-body dark:text-black   justify-between">
+                <h2 className="card-title text-4xl">{room_name}</h2>
+                <p className="text-xl">{description}</p>
+                <div className="md:flex justify-between">
+                    {/* Review Part-------------------> */}
+                    <h3 className="text-lg font-medium dark:text-green-500 text-green-600">Total Reviews: <span className="text-base text-black">
+                        {/* {rating}/5 */}
+                    </span></h3>
+                    <h3 className="text-lg font-medium text-yellow-500">Price Per Night: <span className="text-base text-black">{price_per_night}/- BDT</span></h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RoomsCard;
